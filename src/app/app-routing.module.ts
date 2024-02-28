e67de4a90ef5746c152c2ec6b77d2cd243be9150
Saurabh Tiwari <stjec1804@gmail.com>
2024-02-27 20:34:37 -0500

    first commit
    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' }
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  bootstrap: [AppComponent]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import Validation from './utils/validations';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  form: FormGroup = new FormGroup({
+    lngPlantCapacity: new FormControl(''),
+    capacityUtilization: new FormControl(''),
+    offtakeLongtermCOntracts: new FormControl(''),
+    equityShareHolder1: new FormControl(''),
+    equityShareHolder2: new FormControl(''),
+    
+    interestRateofDebtFinancing: new FormControl(''),
+    debtFiancing: new FormControl(''),
+    equatyFiancing: new FormControl(''),
+    costOfDebt: new FormControl(''),
+    costOfEquaty: new FormControl(''),
+    lngPrice: new FormControl(''),
+    naturalGasPrice: new FormControl(''),
+    naturalGasLiquafactionperunitcost: new FormControl(''),
+    CorporateTaxRate: new FormControl(''),
+    returnSharingtoShareholders: new FormControl(''),
+    OfftakeAgreementofShareholder2: new FormControl(''),
+    longTermContract: new FormControl(''),
+    percentAdditiontoLNGExportpriceinUS: new FormControl(''),
+    JKMLNGprice: new FormControl(''),
+
+  });
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        lngPlantCapacity: ['', Validators.required],
+        capacityUtilization: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        offtakeLongtermCOntracts: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        equityShareHolder1: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        equityShareHolder2: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+     
+        interestRateofDebtFinancing: ['', Validators.required],
+        debtFiancing: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        equatyFiancing: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        costOfDebt: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        costOfEquaty: ['', Validators.required, Validators.maxLength(2), Validators.max(100), Validators.min(0)],
+        lngPrice: ['', Validators.required],
+        naturalGasPrice: ['', Validators.required],
+        CorporateTaxRate: ['', Validators.required],
+        returnSharingtoShareholders: ['', Validators.required],
+        OfftakeAgreementofShareholder2: ['', Validators.required],
+        longTermContract: ['', Validators.required],
+        percentAdditiontoLNGExportpriceinUS: ['', Validators.required],
+        JKMLNGprice: ['', Validators.required],
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+}
